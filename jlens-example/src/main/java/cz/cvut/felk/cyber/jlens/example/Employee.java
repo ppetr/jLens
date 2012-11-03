67dbb8f92e84b1043ea614fa1c8c686d7b335cc3
Petr <petr.mvd@gmail.com>
2012-11-03 21:47:00 +0100

    Added LGPL header to all source files.

--- jlens-example/src/main/java/cz/cvut/felk/cyber/jlens/example/Employee.java
+++ jlens-example/src/main/java/cz/cvut/felk/cyber/jlens/example/Employee.java
@@ -1,3 +1,19 @@
+/*
+    This file is part of jLens.
+
+    jLens is free software: you can redistribute it and/or modify it under the
+    terms of the GNU Lesser General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    jLens is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+    more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with jLens.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package cz.cvut.felk.cyber.jlens.example;
 
 import cz.cvut.felk.cyber.jlens.*;
